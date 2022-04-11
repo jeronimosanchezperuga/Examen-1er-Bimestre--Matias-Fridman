4366df7f2e19e1d81e390865eb118a7f66251a5e
Matifridman <82188442+Matifridman@users.noreply.github.com>
2022-04-11 14:06:36 -0300

    Finished

--- Assets/ejercicioexamen.cs
+++ Assets/ejercicioexamen.cs
@@ -3,11 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ejercicioexamen : MonoBehaviour {
-
+    public string moneda;
+    public int montoaconvertir;
+    int devolucion;
+    int R = 23;
+    int D = 200;
+    int E = 122;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        if (montoaconvertir < 1000)
+        {
+            Debug.Log("Tiene que cambiar mas de $1000");
+        }
+            
+        
+          if (moneda != "D"&&moneda!="R"&&moneda!="E")
+            {
+                Debug.Log("La moneda que ingreso no es valida");
+            }
+        
+        if (moneda == "D"&&montoaconvertir>1000)
+        {
+            devolucion = montoaconvertir / D;
+            Debug.Log(montoaconvertir + " pesos argentinos equivalen a " + devolucion + " " + moneda);
+        }
+        if (moneda == "R"&& montoaconvertir>1000)
+        {
+            devolucion = montoaconvertir / R;
+            Debug.Log(montoaconvertir + " pesos argentinos equivalen a " + devolucion + " " + moneda);
+        }
+        if (moneda == "E"&&montoaconvertir>1000)
+        {
+            devolucion = montoaconvertir / E;
+            Debug.Log(montoaconvertir+" pesos equivalen a "+devolucion+" "+moneda);
+        }
+       
+    }
 	
 	// Update is called once per frame
 	void Update () {
